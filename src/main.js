cfa06ce6f410291f3b60465f1daf192c3dec1c76
qang <qang@its.jnj.com>
2024-09-13 03:11:51 +0800

    unit test signatures

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 const {Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1'); // elliptic curve
+
+const myKey = ec.keyFromPrivate('27004f667e3975948b752da00b9355d56c6d7f5063eb3e6e04b6c15aba639458')
+const myWalletAddress = myKey.getPublic('hex'); // hex format, my walletaddress is my crypto wallet
 
 let javacoin = new Blockchain();
-javacoin.createTransaction(new Transaction('address1', 'address2', 100));
-javacoin.createTransaction(new Transaction('address2', 'address1', 50));
 
-console.log('\n Starting the miner... ')
-javacoin.minePendingTransactions('xavier-address');
-console.log('\nBalance of xavier is', javacoin.getBalanceOfAddress('xavier-address'));
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+javacoin.addTransaction(tx1);
 
-console.log('\n Starting the miner again2... ')
-javacoin.minePendingTransactions('xavier-address');
-console.log('\nBalance of xavier is', javacoin.getBalanceOfAddress('xavier-address'));
+console.log('\n Starting the miner ...');
+javacoin.minePendingTransactions(myWalletAddress);
 
-console.log('\n Starting the miner again3... ')
-javacoin.minePendingTransactions('xavier-address');
-console.log('\nBalance of xavier is', javacoin.getBalanceOfAddress('xavier-address'));
+console.log('\n Balance of xavier is', javacoin.getBalanceOfAddress(myWalletAddress))
