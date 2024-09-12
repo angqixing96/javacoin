f581a500f4ea89c5f5b1db10ae201a56b2794df9
qang <qang@its.jnj.com>
2024-09-13 02:59:51 +0800

    verifying signatures using public private key

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,10 +1,37 @@
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1'); // elliptic curve
 
 class Transaction{
     constructor(fromAddress, toAddress, amount){
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash(){
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString(); // not signing all data in transactions but only hash of transaction
+    }
+
+    signTransaction(signingKey){
+        if(signingKey.getPublic('hex') !== this.fromAddress){
+            throw new Error('You cannot sign transactions for other wallets!');
+        }
+
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64'); // signing the hash of transaction using base64
+        this.signature = sig.toDER('hex'); // signing in special hex format
+    }
+
+    isValid(){
+        if(this.fromAddress === null) return true; // assume transaction is valid when null
+
+        if(!this.signature || this.signature.length === 0){
+            throw new Error('No signature in this transaction'); // needs signature for transaction to be valid
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature); // need to verify if the signature is correct
     }
 }
 
@@ -35,6 +62,16 @@ class Block{
 
         console.log("Block mined: " + this.hash);
         console.log("Block nonce used: " + this.nonce);
+    }
+
+    hasValidTransaction(){ // to verify all transactions in a block
+        for(const tx of this.transactions){
+            if(!tx.isValid()){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
--- src/keygenerator.js
+++ src/keygenerator.js
@@ -6,7 +6,11 @@ const publicKey = key.getPublic('hex');
 const privateKey = key.getPrivate('hex');
 
 console.log();
-console.log('Private key:', privateKey)
+console.log('Private key:', privateKey) // sign transactions and verify balance in wallet
 
 console.log();
 console.log('Public key:', publicKey);
+
+// Private key: 27004f667e3975948b752da00b9355d56c6d7f5063eb3e6e04b6c15aba639458
+
+// Public key: 04b581f5dec6265ebb51b48cc78d688f80c7afda006008fd5c12c787d2ba4eeec5ffbfc2958cf4e83beb741db9032834735c056fbd1ed63ebf0d323c5530f61ae4
